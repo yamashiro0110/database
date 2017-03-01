ab920cc022ad6c7b00220ab2d6e8478317d5adea
yamashiro.ryota <yamashiro0110@gmail.com>
2017-03-02 00:45:47 +0900

    add comment

--- mysql/full_text_index.sql
+++ mysql/full_text_index.sql
@@ -1,5 +1,9 @@
 -- https://dev.mysql.com/doc/refman/5.6/ja/innodb-fulltext-index.html
 
+-- スペース区切りでinsertしないと特定の単語にヒットしない
+-- 5.7.6から組み込みngramパーサが使える
+-- http://mysqlserverteam.com/innodb-%E5%85%A8%E6%96%87%E6%A4%9C%E7%B4%A2-n-gram-parser/
+
 DROP TABLE IF EXISTS full_text_index_table;
 CREATE TABLE full_text_index_table (
   id BIGINT NOT NULL PRIMARY KEY,
@@ -13,7 +17,6 @@ INSERT INTO full_text_index_table(id, title) VALUES
 (3, 'FULLTEXT インデックスは、CREATE TABLE ステートメントの一部として定義することも、あとで ALTER TABLE または CREATE INDEX を使用して追加することもできます。'),
 (4, 'FULLTEXT インデックス は、CREATE TABLE ステートメントの一部として定義することも、あとで ALTER TABLE または CREATE INDEX を使用して追加することもできます。');
 
--- スペース区切りでinsertしないと特定の単語にヒットしない
 SELECT * FROM full_text_index_table WHERE MATCH(title) AGAINST('InnoDB') \G
 SELECT * FROM full_text_index_table WHERE MATCH(title) AGAINST('InnoDB' IN NATURAL LANGUAGE MODE) \G
 
